5c28126062a8cf6cab1a36a4fb98032562f942a2
branham514 <branham.gatheru@student.moringaschool.com>
2023-10-17 06:45:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+// index.js
+document.addEventListener("DOMContentLoaded", function () {
+    const mainElement = document.getElementById('main');
+    if (mainElement) {
+      mainElement.remove();
+    }
+  });
+  // index.js
+document.addEventListener("DOMContentLoaded", function () {
+    // Create a new h1 element and set its text
+    const newHeader = document.createElement('h1');
+    newHeader.id = 'victory';
+    newHeader.textContent = "YOUR-NAME is the champion"; // Replace 'YOUR-NAME' with the actual name
+  
+    // Append the new header to the document body
+    document.body.appendChild(newHeader);
+  });
+  
